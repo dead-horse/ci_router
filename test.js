6eee785f4d1f6b53ea84ef5af242fcdcf04249a3
dead-horse <dead_horse@qq.com>
2012-07-07 06:03:33 +0800

    添加controller子文件夹支持

--- test.js
+++ test.js
@@ -1,15 +1,13 @@
 var connect = require('connect');
 var router = require('./lib/router');
 var app = connect();
+app.use(connect.favicon());
 app.use('/main', function(req, res,next) {
   req.auth = 1;
   next();
 })
 app.use(
   router({
-  rewrite: {
-    '/[a-zA-Z0-9]+': '/main'
-    }
   })
 );
 
